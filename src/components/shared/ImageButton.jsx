bf9a1df33fb9c58de9f11f8da5f465f2f3279fef
retz8 <ekkicb71@gmail.com>
2024-04-05 15:23:21 -0400

    improve mobile board ui little bit

--- src/components/shared/ImageButton.jsx
+++ src/components/shared/ImageButton.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-export default function ImageButton({ icon, text, onClick }) {
+export default function ImageButton({ icon, text = "", onClick }) {
   return (
     <button
       className="bg-slate-100 hover:bg-slate-200
       flex items-center justify-center text-black 
-    py-2 px-2 md:px-4 rounded"
+    py-[6px] md:py-2 px-2 md:px-4 rounded gap-1"
       onClick={onClick}
     >
       {icon}
-      <p className="hidden md:block ml-1">{text}</p>
+      <p className="text-xs sm:text-sm md:text-base">{text}</p>
     </button>
   );
 }
