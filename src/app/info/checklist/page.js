1a513c9061cdd5b1c7c5890ad8bc82c8257a0789
retz8 <ekkicb71@gmail.com>
2024-04-16 22:01:34 -0400

    quick fix

--- src/app/info/checklist/page.js
+++ src/app/info/checklist/page.js
@@ -12,11 +12,7 @@ import {
 
 export default function InfoCheckListPage() {
   return (
-    <section
-      className="flex flex-col items-center pt-2 md:pt-3 lg:pt-4 
-pb-[100px] md:pb-[125px] lg:pb-[150px]
-px-[20px] md:px-[60px] lg:px-[75px]"
-    >
+    <section>
       <InfoTitle title="Things to Do" />
 
       {/* Intro + Description */}
