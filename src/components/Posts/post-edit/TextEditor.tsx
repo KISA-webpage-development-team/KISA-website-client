b0a86be7645d2511f57070cfa9114df37b037284
retz8 <ekkicb71@gmail.com>
2024-06-21 00:49:32 +0900

    quick fix

--- src/components/Posts/post-edit/TextEditor.tsx
+++ src/components/Posts/post-edit/TextEditor.tsx
@@ -1,37 +1,41 @@
-import React from "react";
+import React, { useMemo } from "react";
 import ReactQuill from "react-quill";
 import { TextEditorProps } from "../../../model/props/posts";
 import "react-quill/dist/quill.snow.css";
-
-// this React Quill Module will be moved to separate config file later
-const testReactQuillModules = {
-  toolbar: {
-    container: [
-      [{ size: ["small", false, "large", "huge"] }],
-      [{ list: "ordered" }, { list: "bullet" }],
-      ["bold", "underline", "italic"],
-      [{ align: [] }],
-    ],
-  },
-};
-
-const testReactQuillModulesAdmin = {
-  toolbar: {
-    container: [
-      ["image"],
-      [{ size: ["small", false, "large", "huge"] }],
-      [{ list: "ordered" }, { list: "bullet" }],
-      ["bold", "underline", "italic"],
-      [{ align: [] }],
-    ],
-  },
-};
 
 export default function TextEditor({
   isAdmin,
   text,
   setText,
 }: TextEditorProps) {
+  const testReactQuillModules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ size: ["small", false, "large", "huge"] }],
+          [{ list: "ordered" }, { list: "bullet" }],
+          ["bold", "underline", "italic"],
+          [{ align: [] }],
+        ],
+      },
+    }),
+    []
+  );
+  const testReactQuillModulesAdmin = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          ["image"],
+          [{ size: ["small", false, "large", "huge"] }],
+          [{ list: "ordered" }, { list: "bullet" }],
+          ["bold", "underline", "italic"],
+          [{ align: [] }],
+        ],
+      },
+    }),
+    []
+  );
+
   return (
     <ReactQuill
       theme="snow"
