6f393757245fc7eab9fc820e324aa4b067f415aa
retz8 <ekkicb71@gmail.com>
2024-06-23 12:38:44 +0900

    quick fix

--- src/components/Posts/post-view/PostTitleBar.tsx
+++ src/components/Posts/post-view/PostTitleBar.tsx
@@ -10,12 +10,12 @@ export default function PostTitleBar({
       className="flex justify-start font-medium text-black 
     text-lg sm:text-xl md:text-2xl"
     >
-      {isAnnouncement ? (
-        <p className="text-blue-700 font-bold">[ 공지 ]</p>
-      ) : (
-        <></>
-      )}
-      <h1 className={`${isAnnouncement && "ml-2"} `}>{title}</h1>
+      <span>
+        {isAnnouncement && (
+          <span className="text-blue-700 font-bold">[공지]</span>
+        )}{" "}
+        {title}
+      </span>
     </div>
   );
 }
