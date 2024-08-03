7bfe9d94402dfa64c34463e610ec246c4e265dab
retz8 <ekkicb71@gmail.com>
2024-08-04 02:13:45 +0900

    Quick FIX

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -1,4 +1,4 @@
-import { SWRTokenProvider } from "@/refactor_src/shared/swr";
+import { SWRTokenProvider } from "@/final_refactor_src/lib/swr/providers";
 
 export default function UserLayout({ children }) {
   // "/users" 페이지들은 모두 token을 사용해 GET API call을 하므로,
