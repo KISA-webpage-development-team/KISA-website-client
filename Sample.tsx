aba4e144b7b165dae377ab1cf2be67731cf44b37
retz8 <ekkicb71@gmail.com>
2024-10-06 12:14:22 -0400

    .

---
+++ Sample.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Sample() {
+  const title = "Sample";
+  let value = 0;
+
+  return (
+    <section>
+      <h1>반가워요우</h1>
+
+      <Component />
+      <Component2 title={title} />
+    </section>
+  );
+}
