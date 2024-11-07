c711594a4ad3a9b703a2ba6e9bb8d0c2773cd25b
dkim1112 <de7danielkim@gmail.com>
2024-11-06 23:23:51 -0500

    dongsubbutton change

--- src/components/DongsubButton.tsx
+++ src/components/DongsubButton.tsx
@@ -1,33 +1,11 @@
-"use client";
-import { useState } from "react";
-
-// 몰라도됨
+"use client"; // 몰라도됨
 
 export default function DongsubButton() {
-  // useState: state르ㄹ 관리하는 특별한 리액트 함수
-  const [num, setNum] = useState(1);
-
   function handleOnClick() {
-    setNum(num + 1);
-    console.log("num: ", num);
+    console.log("Click!");
   }
-
-  // = () => {
-
-  // }
-
-  // () {
-
-  // }
 
   const name = "버튼";
 
-  return (
-    <div>
-      <button onClick={handleOnClick}>{name}</button>
-      <p>{num}</p>
-    </div>
-  );
+  return <button onClick={handleOnClick}>{name}</button>;
 }
-
-// Rendering   ----   Function Call
