eae750147108ca77004241f7aa7f6df8d1837a41
retz8 <ekkicb71@gmail.com>
2024-11-24 15:01:45 -0500

    clean up pay and cart logics

--- src/app/pocha/layout.tsx
+++ src/app/pocha/layout.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import PayCartContext from "@/features/pocha/hooks/usePayCart";
 import { SessionProvider } from "next-auth/react";
 
 export default function PochaLayout({ children }) {
-  return (
-    <SessionProvider>
-      <PayCartContext>{children}</PayCartContext>
-    </SessionProvider>
-  );
+  return <SessionProvider>{children}</SessionProvider>;
 }
