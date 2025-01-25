24d235b873e5fedb4d50d7d59805090bd4e3f9bd
retz8 <ekkicb71@gmail.com>
2025-01-25 13:55:51 -0500

    update members page

--- src/app/about/members/page.js
+++ src/app/about/members/page.js
@@ -1,38 +1,56 @@
-import React from "react";
-import InfoTitle from "../../../components/shared/InfoTitle";
+"use client";
+
+import React, { useState } from "react";
+import { Accordion, AccordionItem } from "@nextui-org/react";
 import SubTeamCard from "../../../components/About/SubTeamCard";
 import TeamMembersList from "../../../components/About/TeamMembersList";
-import { members } from "../../../config/static/memberPageData";
-
-export const metadata = {
-  title: "조직도",
-  description:
-    "미시간 대학교 한인 학부 학생회의 멤버들입니다. 회장단, OP팀, PR팀으로 나뉘어져있습니다.",
-};
+import { boardsData } from "../../../config/static/memberPageData";
+import { sejongHospitalBold } from "@/utils/fonts/textFonts";
 
 export default function MemberPage() {
-  const { president, operations, public_relations } = members;
+  const [selectedBoard, setSelectedBoard] = useState(new Set(["members-0"]));
+
+  const accordionItemClass = {
+    base: "py-4",
+    title: `text-4xl font-bold ${sejongHospitalBold.className}`,
+  };
+
+  const getPresident = (board) => {
+    return board.teams.find((team) => team.teamName === "President");
+  };
+
+  const getMembersWithoutPresident = (board) => {
+    return board.teams.filter((team) => team.teamName !== "President");
+  };
 
   return (
-    <section
-      className="flex flex-col items-center pt-2 md:pt-3 lg:pt-4 
-    gap-8 md:gap-16"
-    >
-      <InfoTitle title="24-25 Board" />
-
-      <div className="flex flex-col items-center w-full gap-12">
-        {/* President */}
-        <SubTeamCard role="President" members={president.members} />
-
-        {/* OP + PR */}
-        <div
-          className="flex flex-col md:flex-row md:justify-center 
-        gap-24 md:gap-28 lg:gap-32 w-full "
-        >
-          <TeamMembersList team={operations} name="Operations" />
-          <TeamMembersList team={public_relations} name="Public Relations" />
-        </div>
-      </div>
+    <section className="flex flex-col gap-12">
+      <Accordion
+        selectionMode="multiple"
+        itemClasses={accordionItemClass}
+        defaultSelectedKeys={["members-0"]}
+      >
+        {/*loop through the boardsData and create an AccordionItem for each board*/}
+        {boardsData.map((board, idx) => (
+          <AccordionItem key={`members-${idx}`} title={`${board.year} Board`}>
+            <div className="flex flex-col items-center w-full gap-12">
+              <SubTeamCard
+                role="President"
+                members={getPresident(board).subteams[0]?.members || []}
+              />
+              <div className="flex flex-col md:flex-row md:justify-center gap-24 md:gap-28 lg:gap-32 w-full">
+                {getMembersWithoutPresident(board).map((team) => (
+                  <TeamMembersList
+                    key={team.teamName}
+                    team={team.subteams}
+                    name={team.teamName}
+                  />
+                ))}
+              </div>
+            </div>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </section>
   );
 }
--- src/config/static/memberPageData.js
+++ src/config/static/memberPageData.js
@@ -1,195 +1,403 @@
 // president, operations, public_relations are destructured from members object
 
-const members = {
-  // [ALERT] president's structure is different from the rest of the teams
-  president: {
-    members: [
+import { m } from "framer-motion";
+
+export const boardsData = [
+  {
+    year: "24-25",
+    teams: [
       {
-        name: "Jaemin Jeon",
-        major: "Computer Science",
-        year: "2025",
+        teamName: "President",
+        subteams: [
+          {
+            role: "President",
+            members: [
+              {
+                name: "Jaemin Jeon",
+                major: "Computer Science",
+                year: "2025",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        teamName: "Operations",
+        subteams: [
+          {
+            role: "Vice President",
+            members: [
+              {
+                name: "Jin Wook Shin",
+                major: "Computer Engineering",
+                year: "2026",
+              },
+            ],
+          },
+          {
+            role: "Finance",
+            members: [
+              {
+                name: "Jihyun Park",
+                major: "Economics",
+                year: "2025",
+              },
+            ],
+          },
+          {
+            role: "Operational Support",
+            members: [
+              {
+                name: "Jisang Um",
+                major: "IOE",
+                year: "2026",
+              },
+            ],
+          },
+          {
+            role: "Event Planning",
+            members: [
+              {
+                name: "Yoon Sung Ji",
+                major: "Computer Science",
+                year: "2025",
+              },
+              {
+                name: "Sophia Chang",
+                major: "Robotics",
+                year: "2027",
+              },
+              {
+                name: "Yoonseo Shin",
+                major: "Computer Science",
+                year: "2026",
+              },
+              {
+                name: "Jessica Moon",
+                major: "LSA Undeclared",
+                year: "2027",
+              },
+              {
+                name: "Haeun Lee",
+                major: "BHS",
+                year: "2028",
+              },
+              {
+                name: "Junhee Han",
+                major: "Mech/Chem Engineering",
+                year: "2028",
+              },
+            ],
+          },
+          {
+            role: "Web Development",
+            members: [
+              {
+                isLead: true,
+                name: "Dongsub Kim",
+                major: "Computer Science",
+                year: "2023",
+              },
+              {
+                name: "Jioh In",
+                major: "Computer Science",
+                year: "2028",
+              },
+            ],
+          },
+          {
+            role: "Outreach",
+            members: [
+              {
+                isLead: true,
+                name: "Seungmin Shin",
+                major: "Statistics",
+                year: "2025",
+              },
+              {
+                name: "Wookwan Kwon",
+                major: "Computer Science",
+                year: "2024",
+              },
+              {
+                name: "Junho Lee",
+                major: "Education",
+                year: "2028",
+              },
+              {
+                name: "Dongeun Kim",
+                major: "Data Science",
+                year: "2028",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        teamName: "Public Relations",
+        subteams: [
+          {
+            role: "Vice President",
+            members: [
+              {
+                name: "Stella Han",
+                major: "Computer Science",
+                year: "2025",
+              },
+            ],
+          },
+          {
+            role: "Design",
+            members: [
+              {
+                isLead: true,
+                name: "Kailey Hwang",
+                major: "Computer Science",
+                year: "2026",
+              },
+              {
+                name: "Kelly Koo",
+                major: "BHS",
+                year: "2027",
+              },
+              {
+                name: "Soomin Kang",
+                major: "LSA Undeclared",
+                year: "2028",
+              },
+              {
+                name: "Lauren Kim",
+                major: "Computer Science",
+                year: "2026",
+              },
+            ],
+          },
+          {
+            role: "Social Media/Marketing",
+            members: [
+              {
+                name: "Hyeri Lee",
+                major: "Electrical Engineering",
+                year: "2026",
+              },
+              {
+                name: "Chelsea Kim",
+                major: "Biomedical Engineering",
+                year: "2026",
+              },
+              {
+                name: "Hyo Won Ko",
+                major: "Statistics",
+                year: "2027",
+              },
+              {
+                name: "Cloe Jeong",
+                major: "Psychology & Sociology",
+                year: "2026",
+              },
+              {
+                name: "Younghyun Choi",
+                major: "Psychology",
+                year: "2026",
+              },
+            ],
+          },
+        ],
       },
     ],
   },
-  //   OP team
-  operations: [
-    {
-      role: "Vice President",
-      members: [
-        {
-          name: "Jin Wook Shin",
-          major: "Computer Engineering",
-          year: "2026",
-        },
-      ],
-    },
-    {
-      role: "Finance",
-      members: [
-        {
-          name: "Jihyun Park",
-          major: "Economics",
-          year: "2025",
-        },
-      ],
-    },
-    {
-      role: "Operational Support",
-      members: [
-        {
-          name: "Jisang Um",
-          major: "IOE",
-          year: "2026",
-        },
-      ],
-    },
-    {
-      role: "Event Planning",
-      members: [
-        {
-          name: "Yoonsung Ji",
-          major: "Computer Science",
-          year: "2025",
-        },
-        {
-          name: "Yoonseo Shin",
-          major: "Computer Science",
-          year: "2026",
-        },
-        {
-          name: "Sophia Chang",
-          major: "Robotics",
-          year: "2027",
-        },
-        {
-          name: "Jessica Moon",
-          major: "Undecided",
-          year: "2027",
-        },
-        {
-          name: "Haeun Lee",
-          major: "BHS",
-          year: "2028",
-        },
-        {
-          name: "Junhee Han",
-          major: "Undecided",
-          year: "2028",
-        },
-      ],
-    },
-    {
-      role: "Web Development",
-      members: [
-        {
-          isLead: true,
-          name: "Dongsub Kim",
-          major: "Computer Science",
-          year: "2023",
-        },
-        {
-          name: "Jioh In",
-          major: "Computer Science",
-          year: "2028",
-        },
-      ],
-    },
-    {
-      role: "Outreach",
-      members: [
-        {
-          isLead: true,
-          name: "Seungmin Shin",
-          major: "Statistics",
-          year: "2025",
-        },
-        {
-          name: "Wookwan Kwon",
-          major: "Computer Science",
-          year: "2024",
-        },
-        {
-          name: "Junho Lee",
-          major: "Education",
-          year: "2028",
-        },
-        {
-          name: "Dongeun Kim",
-          major: "Data Science",
-          year: "2028",
-        },
-      ],
-    },
-  ],
-  //   PR team
-  public_relations: [
-    {
-      role: "Vice President",
-      members: [
-        {
-          name: "Seoyeon Han",
-          major: "Computer Science",
-          year: "2025",
-        },
-      ],
-    },
-    {
-      role: "Design",
-      members: [
-        {
-          isLead: true,
-          name: "Jungin Hwang",
-          major: "Computer Science",
-          year: "2026",
-        },
-        {
-          name: "Kelly Koo",
-          major: "BHS & Art and Design",
-          year: "2027",
-        },
-        {
-          name: "Soomin Kang",
-          major: "Undeclared",
-          year: "2028",
-        },
-        {
-          name: "Lauren Kim",
-          major: "Computer Science",
-          year: "2026",
-        },
-      ],
-    },
-    {
-      role: "Social Media/Marketing",
-      members: [
-        {
-          name: "Hyeri Lee",
-          major: "Electrical Engineering",
-          year: "2026",
-        },
-        {
-          name: "Chaeyoung Kim",
-          major: "Biomedical Engineering",
-          year: "2026",
-        },
-        {
-          name: "Hyowon Ko",
-          major: "Statistics",
-          year: "2027",
-        },
-        {
-          name: "Cloe Jung",
-          major: "Psychology",
-          year: "2026",
-        },
-        {
-          name: "Younghyun Choi",
-          major: "Psychology",
-          year: "2026",
-        },
-      ],
-    },
-  ],
-};
-
-export { members };
+  {
+    year: "23-24",
+    teams: [
+      {
+        teamName: "President",
+        subteams: [
+          {
+            role: "President",
+            members: [
+              {
+                name: "Wookwan Kwon",
+                major: "Computer Science",
+                year: "2024",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        teamName: "Operations",
+        subteams: [
+          {
+            role: "Vice President",
+            members: [
+              {
+                name: "Yuri Hong",
+                major: "Electrical Engineering",
+                year: "2026",
+              },
+            ],
+          },
+          {
+            role: "Finance",
+            members: [
+              {
+                name: "Jihyun Park",
+                major: "Economics",
+                year: "2025",
+              },
+            ],
+          },
+          {
+            role: "Operational Support",
+            members: [
+              {
+                name: "Kyungmin Inn",
+                major: "Computer Engineering",
+                year: 2027,
+              },
+            ],
+          },
+          {
+            role: "Event Planning",
+            members: [
+              {
+                name: "Jin Wook Shin",
+                major: "Computer Engineering",
+                year: "2026",
+              },
+              {
+                name: "Jaemin Jeon",
+                major: "Computer Science",
+                year: "2025",
+              },
+              {
+                name: "Yoon Sung Ji",
+                major: "Computer Science",
+                year: "2025",
+              },
+              {
+                name: "Jacob Um",
+                major: "IOE",
+                year: "2026",
+              },
+            ],
+          },
+          {
+            role: "Web Development",
+            members: [
+              {
+                isLead: true,
+                name: "Dongsub Kim",
+                major: "Computer Science",
+                year: "2024",
+              },
+              {
+                name: "Jioh In",
+                major: "Computer Science",
+                year: "2028",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        teamName: "Public Relations",
+        subteams: [
+          {
+            role: "Vice President",
+            members: [
+              {
+                isLead: true,
+                name: "Siwon Lee",
+                major: "Communication & Media",
+                year: "2024",
+              },
+            ],
+          },
+          {
+            role: "Design",
+            members: [
+              {
+                isLead: true,
+                name: "Chaeyeon Park",
+                major: "STAMPS",
+                year: "2025",
+              },
+              {
+                name: "Jade Byeon",
+                major: "STAMPS & Statistics",
+                year: "2026",
+              },
+              {
+                name: "Kailey Hwang",
+                major: "Computer Science",
+                year: "2026",
+              },
+              {
+                name: "Kelly Koo",
+                major: "BHS",
+                year: "2027",
+              },
+            ],
+          },
+          {
+            role: "Social Media/Marketing",
+            members: [
+              {
+                name: "Chelsea Kim",
+                major: "Biomedical Engineering",
+                year: "2026",
+              },
+              {
+                name: "Stella Han",
+                major: "Computer Science",
+                year: "2025",
+              },
+              {
+                name: "Hyo Won Ko",
+                major: "Statistics",
+                year: "2027",
+              },
+              {
+                name: "Hyeri Lee",
+                major: "Electrical Engineering",
+                year: "2026",
+              },
+            ],
+          },
+          {
+            role: "Outreach",
+            members: [
+              {
+                isLead: true,
+                name: "Yoonjoo Lee",
+                major: "Art History & Philosophy",
+                year: "2025",
+              },
+              {
+                name: "Huymin Lee",
+                major: "Data Science & Statistics",
+                year: "2024",
+              },
+              {
+                name: "Jooyoung Chung",
+                major: "Political Science & Pre-Med",
+                year: "2024",
+              },
+              {
+                name: "Seungmin Shin",
+                major: "Computer Science",
+                year: "2025",
+              },
+              {
+                name: "Sophia Jang",
+                major: "Robotics",
+                year: "2027",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
