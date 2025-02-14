2eb5cc06ea929e1ea873dfb9914db54eddbfd922
retz8 <ekkicb71@gmail.com>
2025-02-13 22:29:29 -0500

    .

--- src/apis/images/mutations.ts
+++ src/apis/images/mutations.ts
@@ -11,6 +11,7 @@ export async function putImageToS3(
   file: File
 ): Promise<void> {
   try {
+    console.log("PUT presigned_url", presigned_url);
     await client.put(presigned_url, file, {
       headers: {
         "Content-Type": file.type,
