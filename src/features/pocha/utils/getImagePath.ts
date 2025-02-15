6e3456a0055c6ecd0aed8266d6ee2afa8d179a7c
retz8 <ekkicb71@gmail.com>
2025-02-15 15:15:37 -0500

    set up pocha with correct value

--- src/features/pocha/utils/getImagePath.ts
+++ src/features/pocha/utils/getImagePath.ts
@@ -1,4 +1,4 @@
 export const getMenuImagePath = (menuID: number) => {
-  // Just for the bulgogi case. Else, (menuID != 1) condition should be (menuID != null).
-  return `/pocha/24_last_pocha/${menuID}.png`;
+  const currentPocha = "w25_valentine_pocha";
+  return `/pocha/${currentPocha}/${menuID}.png`;
 };
