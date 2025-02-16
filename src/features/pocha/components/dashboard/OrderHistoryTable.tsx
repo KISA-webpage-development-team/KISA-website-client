5a9ef871ee7ca287ea56f21764c858a4c5ed2067
retz8 <ekkicb71@gmail.com>
2025-02-16 15:09:15 -0500

    tmp pocha

--- src/features/pocha/components/dashboard/OrderHistoryTable.tsx
+++ src/features/pocha/components/dashboard/OrderHistoryTable.tsx
@@ -25,6 +25,7 @@ export default function OrderHistoryTable({
     if (filter === "all") return true;
     return filter === "food" ? !menu.isImmediatePrep : menu.isImmediatePrep;
   });
+  // console.log(filteredOrderHistory);
 
   const calculateTotalSales = () => {
     return filteredOrderHistory
