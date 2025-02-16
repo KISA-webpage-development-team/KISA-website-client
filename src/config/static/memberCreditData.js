8f1a72bccac9c8cb58480ba9bff2f0a6e4865e2c
ysna99 <110143284+ysna99@users.noreply.github.com>
2025-02-16 16:34:33 -0500

    Co-authored-by: hannahlee0717 <hannahlee0717@users.noreply.github.com>

---
+++ src/config/static/memberCreditData.js
@@ -0,0 +1,71 @@
+
+const creditData = [
+  {
+    name: 'Dongsub Kim',
+    email: 'ahjdhlk',
+    role: 'Developer',
+    description:
+      'Developed all CRUD API endpoints for documents on the backend, and an account recovery logic. Also, migrated data from old database to new one.',
+    github: 'sfhjhfks',
+    linkedin: 'skda',
+  },
+
+  {
+    name: 'Jio In',
+    email: 'dad',
+    role: 'Developer',
+    description:
+      'Developed all CRUD API endpoints for documents on the backend, and an account recovery logic.',
+    github: 'ajhdjhk',
+    linkedin: 'dajjdkaj',
+  },
+  {
+    name: 'abc',
+    email: 'dad',
+    role: 'Developer',
+    description:
+      'Developed all CRUD API endpoints for documents on the backend, and an account recovery logic.',
+    github: 'alfkshlks',
+    linkedin: 'fajklkj',
+  },
+  {
+    name: 'def',
+    email: 'dad',
+    role: 'Developer',
+    description:
+      'Developed all CRUD API endpoints for documents on the backend, and an account recovery logic.',
+    github: 'lakjfk',
+    linkedin: 'lkasjfkjs',
+  },
+
+  {
+    name: 'kfskajfl',
+    email: 'dad',
+    role: 'Developer',
+    description:
+      'Developed all CRUD API endpoints for documents on the backend, and an account recovery logic.',
+    github: 'as,dlkasjk',
+    linkedin: 'a.sf.as.l',
+  },
+  {
+    name: 'aflksjfk',
+    email: 'dad',
+    role: 'Developer',
+    description:
+      'Developed all CRUD API endpoints for documents on the backend, and an account recovery logic.',
+    github: 'alskfjkaj',
+    linkedin: 'asfkjklj',
+  },
+
+  {
+    name: 'kalfsjlx,m',
+    email: 'dad',
+    role: 'Developer',
+    description:
+      'Developed all CRUD API endpoints for documents on the backend, and an account recovery logic.',
+    github: 'aslkfjlkj',
+    linkedin: 'askjfljl',
+  },
+];
+
+export { creditData };
