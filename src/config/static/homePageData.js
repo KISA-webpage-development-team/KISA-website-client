d39dba438f1289ef9d9ce866486a7c1314f862dc
ysna99 <110143284+ysna99@users.noreply.github.com>
2025-04-25 12:02:36 -0400

    화란아

--- src/config/static/homePageData.js
+++ src/config/static/homePageData.js
@@ -7,35 +7,8 @@
 // url
 const homeCarouselData = [
   {
-    id: "fa24_kisa_yearbook",
-    title: "KISA Yearbook",
-    desc: (
-      <p>
-        FA24 - WN25 졸업생 선배님들! 미시간 대학교에서 만들었던 소중한 추억과
-        경험들을 KISA Yearbook에 담아 간직해 보세요📽️🎞️ 이어북을 통해 오랫동안
-        기억에 남을 미시간에서의 순간들을 함께 회상해 보는 건
-        어떨까요?❄️선배님들의 학교생활과 노력을 한 권에 모은 특별한 앨범! 얼른
-        신청해 주세요⛄️
-      </p>
-    ),
-    url: "https://www.instagram.com/p/DCmf2f8R-JA/?img_index=1",
-  },
-  {
-    id: "fa24_small_group_recruitment",
-    title: "소그룹 신규 모집",
-    desc: (
-      <p>
-        미시간에서 취미 활동을 함께 할 사람들을 찾고 있나요? 골프, 자동차, 게임
-        볼링, 독서, 음악 등등… 공통된 관심사를 공유하며 돈독한 관계를 쌓을 수
-        있는 “소그룹”이 드디어 열렸습니다! 여러분의 학교생활을 더욱더 윤택하게
-        만들어줄 소그룹, 지금 당장 지원하러 오세요💙
-      </p>
-    ),
-    url: "https://www.instagram.com/p/DAtXy-0OWwa/?img_index=1",
-  },
-  {
-    id: "24-25_new_student_chatroom",
-    title: "[24-25] 신/편입생 톡방",
+    id: 'f25-26_new_student_chatroom',
+    title: '[25-26] 신/편입생 톡방',
     desc: (
       <p>
         미시간 대학교 입학을 축하드립니다!! KISA에서 준비한 신/편입생 톡방에
@@ -44,8 +17,21 @@ const homeCarouselData = [
         오픈채팅방 비밀번호는 @kisa_michigan 인스타 디엠으로 물어봐주세요!
       </p>
     ),
-    url: "https://open.kakao.com/o/gFbE6Hng",
+    url: 'https://open.kakao.com/o/gqnJuhsh',
   },
+  {
+    id: 'fa24_small_group_recruitment',
+    title: '소그룹 신규 모집',
+    desc: (
+      <p>
+        미시간에서 취미 활동을 함께 할 사람들을 찾고 있나요? 골프, 자동차, 게임
+        볼링, 독서, 음악 등등… 공통된 관심사를 공유하며 돈독한 관계를 쌓을 수
+        있는 “소그룹”이 드디어 열렸습니다! 여러분의 학교생활을 더욱더 윤택하게
+        만들어줄 소그룹, 지금 당장 지원하러 오세요💙
+      </p>
+    ),
+    url: 'https://www.instagram.com/p/DAtXy-0OWwa/?img_index=1',
+  }
 ];
 
 // <homeQuickLinksData>
