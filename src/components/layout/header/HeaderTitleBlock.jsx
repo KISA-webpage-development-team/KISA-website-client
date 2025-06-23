e8e78cd17c80a947e9f6779218f1f47b902c7dd2
retz8 <ekkicb71@gmail.com>
2025-06-22 22:10:18 -0700

    feat: add new jobs-curator page with header setup

--- src/components/layout/header/HeaderTitleBlock.jsx
+++ src/components/layout/header/HeaderTitleBlock.jsx
@@ -1,12 +1,12 @@
-import Link from 'next/link';
-import WebLogo from './WebLogo';
-import WebTitle from './WebTitle';
+import Link from "next/link";
+import WebLogo from "./WebLogo";
+import WebTitle from "./WebTitle";
 
 export default function HeaderTitleBlock() {
   return (
-    <div className='flex items-center gap-5'>
-    <WebLogo />
-    <WebTitle />
+    <div className="flex items-center gap-5">
+      <WebLogo />
+      <WebTitle />
     </div>
   );
 }
