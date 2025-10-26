c9d9a4fd09f89b5b0ab1ff7dccc85df471c1533a
retz8 <ekkicb71@gmail.com>
2025-10-26 12:03:05 +0900

    feat: wip - implement previous pocha list

--- src/apis/pocha/queries.ts
+++ src/apis/pocha/queries.ts
@@ -2,6 +2,7 @@ import client from "@/lib/axios/client";
 import {
   MenuByCategory,
   PochaInfo,
+  PochaInfoWithoutStatus,
   Cart,
   Orders,
   OrderHistory,
@@ -40,17 +41,23 @@ export async function getPochaInfo(date: Date): Promise<PochaInfo> {
   }
 }
 
-export async function getPochaInfoMock(date: Date) {
-  const mockPochaInfo: PochaInfo = {
-    pochaID: 1,
-    startDate: new Date(),
-    endDate: new Date(new Date().getTime() + 4 * 60 * 60 * 1000),
-    title: "Halloween Pocha",
-    description:
-      "할로윈 포차 입니다. 한잔 포차에서 11월 2일 진행될 예정입니다! ^^",
-    ongoing: true,
-  };
-  return mockPochaInfo;
+/**
+ * @desc Fetch all previous pocha
+ * @route GET /pocha/previous/
+ */
+// START HERE!
+
+export async function getPreviousPochaList(
+  date: Date
+): Promise<PochaInfoWithoutStatus[]> {
+  const url = `/pocha/previous/?date=${date.toISOString().split(".")[0]}`;
+
+  try {
+    const response = await client.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error("Error fetching previous pocha information");
+  }
 }
 
 /**
