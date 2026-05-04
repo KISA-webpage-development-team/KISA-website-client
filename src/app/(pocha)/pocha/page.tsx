337ab1c2d3721c4f116968af9c7e801f99785808
retz8 <ekkicb71@gmail.com>
2026-05-04 11:32:22 +0900

    chore: change tab to lg

--- src/app/(pocha)/pocha/page.tsx
+++ src/app/(pocha)/pocha/page.tsx
@@ -172,7 +172,7 @@ export default function PochaPage() {
 
         {/* Sticky tabs */}
         <div className="sticky top-0 z-[45] bg-surface">
-          <TabsList variant="underline" size="md" fullWidth>
+          <TabsList variant="underline" size="lg" fullWidth>
             <TabsTrigger value="menu" variant="underline">
               Menu
             </TabsTrigger>
